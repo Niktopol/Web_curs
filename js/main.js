851aae365adf483dad94bc2eaa4097f0e3feeca3
Niktopol <kirill-22ru@yandex.ru>
2023-11-23 21:46:04 +0300

    footer

--- js/main.js
+++ js/main.js
@@ -21,13 +21,21 @@ document.querySelectorAll(".serv_back").forEach(function(elem){
 let scroll = document.querySelectorAll("#popular button");
 let scroll_panel = 0;
 let panels = document.querySelectorAll("#popular > div > div");
+let pos = document.querySelector("#popular span");
+pos.textContent = "1/"+panels.length;
 scroll[0].addEventListener("click", function(){
+    scroll[0].disabled = true;
+    setTimeout(function(){scroll[0].disabled = false}, 300)
     panels[scroll_panel].className = "hidden_r";
     scroll_panel = scroll_panel-1 < 0 ? panels.length-1 : scroll_panel-1;
     panels[scroll_panel].className = "visible_r";
+    pos.textContent = scroll_panel+1+"/"+panels.length;
 });
 scroll[1].addEventListener("click", function(){
+    scroll[1].disabled = true;
+    setTimeout(function(){scroll[1].disabled = false}, 300)
     panels[scroll_panel].className = "hidden_l";
     scroll_panel = scroll_panel+1 == panels.length ? 0 : scroll_panel+1;
     panels[scroll_panel].className = "visible_l";
+    pos.textContent = scroll_panel+1+"/"+panels.length;
 });
