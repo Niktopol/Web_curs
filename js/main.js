e8bf11dc781775d04d7b444a8d8e23340cca0070
Kirill <kirill-22ru@yandex.ru>
2023-11-23 19:15:11 +0300

    Slider'n'footer

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 "use strict";
+document.querySelector("#clear_btn").addEventListener("click", function(){
+    document.querySelector("#search input").value = "";
+});
 document.querySelectorAll(".serv_fr button").forEach(function(elem){
     elem.addEventListener("click", function(){
         let fr = elem.parentNode.parentNode.querySelector(".serv_fr");
@@ -14,4 +17,17 @@ document.querySelectorAll(".serv_back").forEach(function(elem){
         fr.className = fr.className.split(" ")[0]+ " " + "shown";
         back.className = back.className.split(" ")[0]+ " " + "hidden";
     });
+});
+let scroll = document.querySelectorAll("#popular button");
+let scroll_panel = 0;
+let panels = document.querySelectorAll("#popular > div > div");
+scroll[0].addEventListener("click", function(){
+    panels[scroll_panel].className = "hidden_r";
+    scroll_panel = scroll_panel-1 < 0 ? panels.length-1 : scroll_panel-1;
+    panels[scroll_panel].className = "visible_r";
+});
+scroll[1].addEventListener("click", function(){
+    panels[scroll_panel].className = "hidden_l";
+    scroll_panel = scroll_panel+1 == panels.length ? 0 : scroll_panel+1;
+    panels[scroll_panel].className = "visible_l";
 });
