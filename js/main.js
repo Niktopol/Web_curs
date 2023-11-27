0c9f442b8e8e24b0f3ff7197c35376e8cb40f124
Niktopol <kirill-22ru@yandex.ru>
2023-11-27 18:47:15 +0300

    Main page

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@ function toggleMenuFold(){
         catalogBtn.className = "unfolded";
         catalogBtn.querySelector("#burger").className = "unfolded";
         document.querySelector("#menu").className = "unfolded";
+        document.body.className = "unfolded";
     }else{
         catalogBtn.className = "folded";
         catalogBtn.querySelector("#burger").className = "folded";
         document.querySelector("#menu").className = "folded";
+        document.body.className = "folded";
     }
 }
 document.querySelector("#catalog").addEventListener("click", toggleMenuFold);
